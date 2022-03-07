478c0eb42d5f873714b386c549dee24963f93bd1
Eric Leroy-Terquem <eric.leroy-terquem@enercoop.org>
2022-03-08 00:22:09 +0100

    add caching

--- app/ressources/poke/api.rb
+++ app/ressources/poke/api.rb
@@ -1,11 +1,15 @@
 module Poke::Api
   def self.fetch(params)
     url = "https://pokeapi.co/api/v2/pokemon/#{params.downcase}"
-    response = Faraday.get(url)
-    if response.status == 200
-      JSON.parse(response.body)
-    else
-      nil
+
+    Rails.cache.fetch(url, expires_in: 1.hour) do
+      response = Faraday.get(url)
+
+      if response.status == 200
+        JSON.parse(response.body)
+      else
+        nil
+      end
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module PokeApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.cache_store = :memory_store, { size: 64.megabytes }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
