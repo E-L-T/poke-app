619a27d82b8ba97cc804ce640a12cc34c1570600
Eric Leroy-Terquem <eric.leroy-terquem@enercoop.org>
2022-03-09 00:41:00 +0100

    add validation

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,9 +4,29 @@ class PokemonsController < ApplicationController
   end
 
   def show
-    @pokemon = Poke::Api.fetch(params[:name])
+    if valid_params(params[:name])
+      @pokemon = Poke::Api.fetch(params[:name])
+    else
+      redirect_to root_path and return
+    end
+
     unless @pokemon
       flash.now[:notice] = 'Je ne connais pas ce Pokemon'
     end
+  end
+
+  private
+
+  def valid_params(name)
+    if name.blank?
+      flash[:notice] = 'Tu dois entrer un nom de Pokemon'
+      false
+    elsif name.length > 100
+      flash[:notice] = 'Ce nom de Pokemon est trop long'
+      false
+    else
+      true
+    end
+
   end
 end
